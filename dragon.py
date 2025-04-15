36606f872061d16005f5f9646f14de4bf2eadfaa
KOJIN <dnwla0412@gmail.com>
2025-04-15 17:37:36 +0000

    드래곤

---
+++ dragon.py
@@ -0,0 +1,36 @@
+import time
+import random as ran
+
+print("""You are in a land full of dragons.
+      In front of you, You see two caves. 
+      In one cave, the dragon is friendly and will share his treasure with you.
+      The other dragon is greedy and hungry, and will eat you on sight.""")
+
+while True:
+    print("Which cave will you go into? (1 or 2)")
+    cave=ran.randint(1,2)
+
+    choice_num=int(input())
+    
+    time.sleep(1)
+    print("You approach the cave . . .")
+    time.sleep(2)
+    print("It is dark and spooky . . .")
+    time.sleep(2)
+    print("A large dragon jumps out in front of you! He opens his jaws")
+    print("and . . .")
+    time.sleep(4)
+    
+    if choice_num == cave :
+        print("Gives you his treasure!")
+    else:
+        print("Gobbles you down in one bite!")
+    
+    print("Do you want to play again? (yes or no)")
+
+    answer=input()
+
+    if answer.lower() == "yes" :
+        continue
+    if answer.lower() == "no" :
+        break
---
+++ guess.py
@@ -0,0 +1,28 @@
+import random as ran
+
+print("안녕하세요! 이름이 뭐니?")
+
+name=input()
+
+print(f"{name}, 1에서 20 사이의 숫자를 생각하고 있어요.")
+print("맞춰보세요.")
+
+num=ran.randint(1, 20)
+count=1
+
+while True:
+    num_user=int(input())
+    
+    if num_user > num :
+        print("너무 높은 숫자네요.")
+        print("맞춰 보세요.")
+        count+=1
+        continue
+    if num_user < num :
+        print("너무 낮은 숫자네요.")
+        print("맞춰 보세요.")
+        count+=1
+        continue
+    if num_user == num :
+        print(f"잘했어요, {name}! {count}번 만에 제 숫자를 맞혔네요!")
+        break
