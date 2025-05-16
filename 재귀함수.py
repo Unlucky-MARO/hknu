e13cf21bc2f9b15a1a292800c08a586a9f8461b0
KOJIN <dnwla0412@gmail.com>
2025-05-16 22:28:12 +0900

    codingalzi

--- 재귀함수.py
+++ 재귀함수.py
@@ -102,3 +102,78 @@ for n in range(10):
     print(fibonacci(n), end=', ')
 
 print('...')
+
+#콜라츠 추측
+#독일 수학자 콜라츠(Collatz -> L.)가 1937년에 아래 알고리즘을
+#얼마나 많이 반복하면 최종적으로 숫자 1에 다다를 것인가를 질문했다.
+#주어진 숫자가 짝수면 2로 나눈다.
+#주어진 숫자가 홀수면 3배한 후 1을 더한다.
+
+#콜라츠 알고리즘을 재귀 함수로 구현할 수 있다.
+#기저 조건: num == 1
+#num % 2 == 0: 짝수인 경우. 2로 나누기
+#기타else: 홀수 인 경우. 3배 더하기 1.
+
+def collatz(num):
+    if num == 1:
+        print(1)
+    elif num%2 == 0:
+        print(num, end=' -> ')
+        collatz(num//2)
+    else:
+        print(num, end=' -> ')
+        collatz(num*3 + 1)
+
+#재귀 호출 횟수를 반환값으로 지정할 수 있다.
+#n == 1: 재귀 호출 없음. 0 반환.
+#짝수인 경우: 2로 나눈 값에 대한 재귀 호출 횟수 더하기 1
+#홀수인 경우: 세 배 더하기 1에 대한 재귀 호출 횟수 더하기 1
+
+def collatz_count(num):
+    if num == 1:
+        return 0
+    elif num % 2 == 0:
+        return collatz_count(num//2) + 1
+    else:
+        return collatz_count(num*3 + 1) + 1
+    
+#하노이의 탑
+#for, while 반복문을 이용하여 해결하기 어려운 문제를 
+#재귀 알고리즘으로 상대적으로 훨씬 간단하게 해결하는 
+#예제로 하노이의 탑 문제를 다룬다.
+
+#하노이의 탑Tower of Hanoi 문제는 세 개의 기둥 중에 
+#하나의 기둥에 쌓여 있는 다양한 크기의 원판들을 다른 기둥으로 
+#옮기는 게임이다. 단, 원판 이동 중에 아래 제한조건들을 
+#반드시 지켜야 한다.
+
+#한 번에 한개의 원판만 옮긴다.
+#큰 원판이 그보다 작은 원판 위에 위치할 수 없다.
+
+#참고: 일반적으로 원판이 n개 일 때, 2^n - 1번의 이동으로 
+#원판을 모두 옮길 수 있다. 참고로 64개의 원판을 옮기는 데 총 
+#2^64 -1번 원판을 움직여야 하고, 1초에 하나의 원판을 옮긴다고 
+#가정했을 때 5,849억년 정도 걸린다.
+
+#재귀 알고리즘
+#n-1 개의 원판을 중간 지점의 위치에 옮긴다.
+#가장 큰 원판을 목적지로 옮긴다.
+#중간 지점에 위치한 n-1 개의 원판을 목적지로 옮긴다.
+#위 재귀 알고리즘의 종료조건은 n=1일 때이며, 
+#이때는 하나의 원판을 목적지로 옮기기만 하면 된다.
+
+#height: 원판 개수
+#from_pole: 출발 기둥
+#with_pole: 중간 지점 기둥
+#to_pole: 목적지 기둥
+
+def move_tower(height, from_pole, to_pole, with_pole):
+    if height >= 1:
+        move_tower(height - 1, from_pole, with_pole, to_pole)
+        move_disk(from_pole, to_pole)
+        move_tower(height - 1, with_pole, to_pole, from_pole)
+
+def move_disk(from_p, to_p):
+    print(f"{from_p}에서 {to_p}로 탑 원판 옮기기")
+
+move_tower(4, "A", "B", "C")
